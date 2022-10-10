8fb1c88ae4beebda7fe81f0ff9aa832efc744ebd
Surenthirakumar <112889307+Surenthirakumar@users.noreply.github.com>
2022-10-10 10:55:26 +0530

    Add files via upload

---
+++ SUMOF2NOS.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+   int a, b, *p, *q, sum;
+
+   printf("Enter the  two integers \n");
+   scanf("%d%d", &a, &b);
+
+   p = &a;
+   q = &b;
+
+   sum = *p + *q;
+
+   printf("Sum of the numbers = %d\n", sum);
+
+   return 0;
+}
+
+
